dd8175f22e655587a7089f583791d99ab14fc71a
maxim <estonmax@gmail.com>
2020-11-07 15:02:50 +0300

    created NotWorking test

---
+++ server/src/test/java/TestLess6_WhyNotWorking.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/*
+Не могу понять, почему не работает этот тест для задания №1
+ */
+
+@RunWith(Parameterized.class)
+public class TestLess6_WhyNotWorking {
+
+}
