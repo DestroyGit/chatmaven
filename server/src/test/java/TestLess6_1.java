053214f4ae9fe9775cc3f2aff46879bf9648c763
maxim <estonmax@gmail.com>
2020-11-07 16:02:45 +0300

    not use

---
+++ server/src/test/java/TestLess6_1.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/*
+Тесты для задания 1
+ */
+
+@RunWith(Parameterized.class)
+public class TestLess6_1 {
+
+
+
+}
---
+++ server/src/test/java/TestLess6_2.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+// Тесты для задания 2
+
+public class TestLess6_2 {
+
+}
